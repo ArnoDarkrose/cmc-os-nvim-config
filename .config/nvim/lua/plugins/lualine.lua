ce55cf46862e58f15dd813707af8a8994d8d5a15
Svyatoslav <slava30727@github.com>
2024-09-11 10:30:30 +0000

    Autocompletion, renaming, hints

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,6 +3,9 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
         options = { theme = "horizon" },
+        sections = {
+            lualine_x = { require("action-hints").statusline },
+        },
     },
     config = function(spec, opts)
         require("lualine").setup(opts)
---
+++ .config/nvim/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int number;
+
+void
+function(int number)
+{
+    number += 1;
+}
+
+int
+main()
+{
+    return EXIT_SUCCESS;
+}
